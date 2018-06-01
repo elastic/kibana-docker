86311089d87b816d0311e5e659d27a4d103cd371
Toby McLaughlin <toby@jarpy.net>
2018-06-01 14:30:28 +1000

    Test files are GID 0

--- tests/test_files.py
+++ tests/test_files.py
@@ -13,3 +13,10 @@ def test_opt_kibana_is_a_symlink_to_usr_share_kibana(kibana):
 def test_all_files_in_optimize_directory_are_owned_by_kibana(kibana):
     bad_files = kibana.stdout_of('find /usr/share/kibana/optimize ! -user kibana').split()
     assert len(bad_files) is 0
+
+
+def test_all_files_in_kibana_directory_are_gid_zero(kibana):
+    suspect_files = kibana.stdout_of('find /usr/share/kibana ! -gid 0').split()
+    # Files can be created at runtime in /usr/share/kibana/data. Don't worry about them.
+    relevant_files = [f for f in suspect_files if not f.startswith('/usr/share/kibana/data/')]
+    assert len(relevant_files) is 0
